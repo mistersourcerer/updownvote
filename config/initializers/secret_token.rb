ad1edb63abc5294d0fc3ce40db201d95734c7f93
Ricardo Valeriano <ricardo.valeriano@gmail.com>
2012-03-04 11:05:37 -0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Updownvote::Application.config.secret_token = 'eb096797efb9a602e0ba06510e86fb9963eaecf7b52ad637b898e68683a58eeafcc66522596b01baed9ce62c788f31074c121247206900492ba2d609b91bb9eb'
