06efebafba72fa86c1469561b4c29347328caae2
Ricardo Valeriano <ricardo.valeriano@gmail.com>
2012-03-16 15:04:07 -0300

    macro to summoning authenticator

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,11 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   private
+  def self.uses_authenticator(method_name = :auth)
+    define_method method_name do
+      @authenticator ||= Authenticator.new(session, request.env["omniauth.auth"])
+    end
+  end
 
   def authenticator
     @authenticator ||= Authenticator.new(session, request.env["omniauth.auth"])
